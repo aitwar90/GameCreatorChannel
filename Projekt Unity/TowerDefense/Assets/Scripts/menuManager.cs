d1d86c421cc203f091a0a853a295fc4545255779
tomasz.golacik <tomasz.golacik@tomorrow.pro>
2020-09-07 21:20:43 +0200

    Nazwy klas piszemy z dużych liter :)

--- Projekt Unity/TowerDefense/Assets/Scripts/menuManager.cs
+++ Projekt Unity/TowerDefense/Assets/Scripts/menuManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class menuManager : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
