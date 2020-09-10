e381ac467b7c9b6472c5a05bab0eaeca65507caa
Marcin Zawadzki <nicram01@tlen.pl>
2020-09-10 16:41:37 +0200

    Dodanie nowej ScenMenu i poprawa nacmesch

--- Projekt Unity/TowerDefense/Assets/Scripts/menuManager.cs
+++ Projekt Unity/TowerDefense/Assets/Scripts/menuManager.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void dupa(int jakDuza) {
+             Debug.Log("dupaaaaaa");
     }
 }
+ 
