329acc88f7d705673afd3658085e5865390a9d3e
tomasz.golacik <tomasz.golacik@tomorrow.pro>
2020-12-08 11:37:38 +0100

    Poprawki i napisanie funkcji włączających muzykę

--- Projekt Unity/TowerDefense/Assets/Scripts/MuzykaScript.cs
+++ Projekt Unity/TowerDefense/Assets/Scripts/MuzykaScript.cs
@@ -7,18 +7,37 @@ public class MuzykaScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public AudioSource muzykaTła;
-    [Tooltip("Nazwa musi mieć szablon x_y przy czym x oznacza przeznaczenie clipu zaś y kolejny numer np. Tło_1 oznacza pierwszy utwór przeznaczony dla tła. Typy: Tło, AmbientWGrze, AtakNPCDystans, AtakNPCZwarcie, AtakBJeden, AtakBObszar, AtakBAll, ŚmiercNPC, ŚmiercB, Poruszanie")]
+    [Tooltip("Nazwa musi mieć szablon x przy czym x oznacza przeznaczenie clipu zaś y kolejny numer np. Tło oznacza pierwszy utwór przeznaczony dla tła. Typy: Tło, AmbientWGrze, AtakNPCDystans, AtakNPCZwarcie, AtakBJeden, AtakBObszar, AtakBAll, ŚmiercNPC, ŚmiercB, Poruszanie")]
     public StrukturaAudio[] clipyAudio;
     public delegate void UstawGłośność(sbyte wartość);
     public UstawGłośność ustawGłośność;
+    private byte[] indeksyMuzyki = null;
 
     void Awake()
     {
         if (clipyAudio != null && clipyAudio.Length > 0)
         {
             ustawGłośność += UstawGłośnośćTła;
-            muzykaTła.clip = clipyAudio[0].clip;
-            muzykaTła.Play();
+            WłączWyłączClip(ref muzykaTła, true, "Tło", false);
+        }
+    }
+    void Start()
+    {
+        string lastString = null;
+        if (clipyAudio != null && clipyAudio.Length > 0)
+        {
+            lastString = clipyAudio[0].nazwa;
+            List<byte> tByte = new List<byte>();
+            tByte.Add(0);
+            for (byte i = 1; i < clipyAudio.Length; i++)
+            {
+                if(clipyAudio[i].nazwa != lastString)
+                {
+                    lastString = clipyAudio[i].nazwa;
+                    tByte.Add(i);
+                }
+            }
+            indeksyMuzyki = tByte.ToArray();
         }
     }
     public void UstawGłośnośćGry(int val)   //Ta metoda powinna być wywoływana w opcjach aplikacji
@@ -36,12 +55,51 @@ public class MuzykaScript : MonoBehaviour
             ustawGłośność((sbyte)val);
         }
     }
-    /*
-    public AudioClip ZwróćSzukanyClip(string typ)
+    public void WłączWyłączClip(ref AudioSource ado, bool czyWłączyć, string typ = "", bool czyOneShoot = false)
     {
-
+        if(!czyWłączyć)
+        {
+            ado.Stop();
+        }
+        else
+        {
+            ado.clip = ZwróćSzukanyClip(typ);
+            if(czyOneShoot)
+            {
+                ado.PlayOneShot(ado.clip);
+            }
+            else
+            {
+                ado.Play();
+            }
+        }
     }
-    */
+    private AudioClip ZwróćSzukanyClip(string typ)   //Funkcja zwraca klip w zależności od podanego w parametrze typu
+    {
+        if(indeksyMuzyki != null)
+        {
+            for(byte i = 0; i < indeksyMuzyki.Length; i++)
+            {
+                if(clipyAudio[indeksyMuzyki[i]].nazwa == typ)
+                {
+                    if(indeksyMuzyki[i] == indeksyMuzyki.Length-1)  //Jeśli typ jest jedyny i ostatni w tablicy
+                    {
+                        return clipyAudio[indeksyMuzyki[i]].clip;
+                    }
+                    else if(indeksyMuzyki[i+1] - indeksyMuzyki[i] == 1) //Jeśli typ jest jedyny
+                    {
+                        return clipyAudio[indeksyMuzyki[i]].clip;
+                    }
+                    else    //Obsługa domyślna
+                    {
+                        byte x = (byte)Random.Range(indeksyMuzyki[i], indeksyMuzyki[i+1]-1);
+                        return clipyAudio[x].clip;
+                    }
+                }
+            }
+        }
+        return null;
+    }
     public void UstawGłośnośćTła(sbyte wartość)
     {
         muzykaTła.volume = wartość / 100.0f;
