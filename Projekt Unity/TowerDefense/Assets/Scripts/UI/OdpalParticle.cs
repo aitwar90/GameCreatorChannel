80e1c981618443cd0327646858a0b16e8c159a01
tomekGol <tomek.golacik@gmail.com>
2021-04-16 14:12:34 +0200

    Dodane particle do menu

---
+++ Projekt Unity/TowerDefense/Assets/Scripts/UI/OdpalParticle.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class OdpalParticle : MonoBehaviour
+{
+    private EventSystem eventSystem;
+    private bool czyOdpalony = false;
+    private GameObject pSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventSystem = PomocniczeFunkcje.eSystem;
+        pSystem = this.GetComponentInChildren<ParticleSystem>().gameObject;
+        pSystem.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!czyOdpalony)
+        {
+            if(this.gameObject == eventSystem.currentSelectedGameObject)
+            {
+                czyOdpalony = true;
+                pSystem.SetActive(true);
+            }
+        }
+        else
+        {
+            if(this.gameObject != eventSystem.currentSelectedGameObject)
+            {
+                czyOdpalony = false;
+                pSystem.SetActive(false);
+            }
+        }
+    }
+}
